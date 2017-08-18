2d52f1c09bf83334d146a140070a4e3c33cbc8c6
Vishal Vishwanathan <vishalviswanathan9@gmail.com>
2017-08-18 14:54:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 from socket import *
 
 HOST = "172.16.23.173" #local host
+
 PORT = 7000 #open port 7000 for connection
+
 s = socket(AF_INET, SOCK_STREAM)
+
 s.bind((HOST, PORT))
+
 s.listen(1) #how many connections can it receive at one time
+
 conn, addr = s.accept() #accept the connection
+
 print "Connected by: " , addr #print the address of the person connected
+
 while True:
     data = conn.recv(1024) #how many bytes of data will the server receive
     print "Received: ", repr(data)
     reply = raw_input("Reply: ") #server's reply to the client
     conn.sendall(reply)
+
 conn.close()
